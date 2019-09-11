ab9d4bbe78d57172d7c217ecc60159efaa04ee11
Sephka <32805735+Sephka@users.noreply.github.com>
2019-09-10 18:37:03 -0700

    Python Homework Basic Finished

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,24 +2,15 @@
 import os
 import csv
 
-# Declaring CSV Columns
-voter_id = []
-county = []
-
 # Declaring variables
 votes_cast = 0
-candidate_list = set()
-cand_per_vote = 0
-cand_total_vote = 0
 winner = ''
 line_break = "-"*30
 dct = {}
-votes = 0
+candidate_list = []
 
 # Path to collect data from csv in resources folder
 poll_csv = os.path.join('Resources','election_data.csv')
-
-# Saving space for functions if necessary
 
 # Read that CSV file! Go go go!
 with open(poll_csv, newline='') as csvfile:
@@ -35,35 +26,29 @@ with open(poll_csv, newline='') as csvfile:
 		else:
 			dct[row['Candidate']][0] += 1
 
-		# Creating list of unique candidates using previously declared set variable
-		candidate_list.add(row['Candidate'])
-
 		# Tracking total number of votes cast
 		votes_cast = votes_cast + 1
 
 
-# Creates and writes variables to a text file
-
-
-print(candidate_list)
-print(votes_cast)
-print(dct)
-print(dct['Khan'][0])
-
+# Formatting vote percentages into percentages properly
 for person in dct:
-	percentage = dct[person][0] / votes_cast
+	percentage = "{:.3f}%".format(dct[person][0] / votes_cast * 100)
 	dct[person].append(percentage)
 
-print(dct)
-
+# Grabbing the highest vote count, associating it with the correct key, to be used when printing out the text file.
 highest_votes = max([dct[i] for i in dct], key = lambda x: x[1])[0]
 winner = [i for i in dct if dct[i][0] == highest_votes]
-print(winner[0])
+
+# Grabbing the keys from the dictionary to use in writing the text file.
+for cand in dct:
+	candidate_list.append(cand)
+
+# One lining the text file yaaay
+txt_file_info = f"Election Results"'\n'f"{line_break}"'\n'f"Total Votes: {votes_cast}"'\n'f"{line_break}"'\n'f"{candidate_list[0]}: {dct[candidate_list[0]][1]} ({dct[candidate_list[0]][0]})"'\n'f"{candidate_list[1]}: {dct[candidate_list[1]][1]} ({dct[candidate_list[1]][0]})"'\n'f"{candidate_list[2]}: {dct[candidate_list[2]][1]} ({dct[candidate_list[2]][0]})"'\n'f"{candidate_list[3]}: {dct[candidate_list[3]][1]} ({dct[candidate_list[3]][0]})"'\n'f"{line_break}"'\n'f"Winner: {winner[0]}"
+print(txt_file_info)
 
 
-'''
 # Creating text file and adding information
-textFile = open("poll_file.txt","w")
+text_file = open("poll_file.txt","w")
 with open("poll_file.txt", "w") as att_file:
 	att_file.write(txt_file_info)
-'''
